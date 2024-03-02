80789cd3849727643bfb89d71ec1b875c605ce99
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-03-02 15:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 //complete this code
-class Person {}
+class Person {
+	 constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age;
+    }
 
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+	 constructor(name, age) {
+        super(name, age);
+    }
+
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+	 constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+const person = new Person("John", 25);
+console.log(person.name);
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
